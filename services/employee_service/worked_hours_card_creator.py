ec15673313b6db5b3269662d4103022c139c95fc
NiXerasse <nixerasse@gmail.com>
2025-01-16 11:08:12 +0300

    fixed 42 month

--- services/employee_service/worked_hours_card_creator.py
+++ services/employee_service/worked_hours_card_creator.py
@@ -13,19 +13,30 @@ from lxml import etree
 from locales.locales import gettext
 from logger.logger import logger
 
-svg_file_name = file_path = os.path.join(os.path.dirname(__file__), 'worked_hours.svg')
+svg_file_name = os.path.join(os.path.dirname(__file__), 'worked_hours.svg')
+svg42_file_name = os.path.join(os.path.dirname(__file__), 'worked_hours_42.svg')
 
 class WorkedHoursCardCreator:
-    tree = None
+    tree_35 = None
+    tree_42 = None
     namespace = {"svg": "http://www.w3.org/2000/svg"}
 
 
-    def __init__(self, locale='en'):
-        if WorkedHoursCardCreator.tree is None:
+    def __init__(self, month, year, locale='en'):
+        if WorkedHoursCardCreator.tree_35 is None:
             logger.warning('Reading svg disk file')
             WorkedHoursCardCreator.tree = etree.parse(svg_file_name)
+            WorkedHoursCardCreator.tree_42 = etree.parse(svg42_file_name)
 
-        self.svg_tree = copy.deepcopy(WorkedHoursCardCreator.tree)
+        self.month = month
+        self.year = year
+        self.first_weekday, self.days_in_month = calendar.monthrange(self.year, self.month)
+        self.card_size = 35
+        if self.first_weekday + self.days_in_month > self.card_size:
+            self.card_size += 7
+
+        self.svg_tree = copy.deepcopy(
+            WorkedHoursCardCreator.tree_35 if self.card_size == 35 else WorkedHoursCardCreator.tree_42)
         self.root = self.svg_tree.getroot()
         self.locale = locale
 
@@ -45,10 +56,9 @@ class WorkedHoursCardCreator:
         tspan_element = element.find("svg:tspan", namespaces=WorkedHoursCardCreator.namespace)
         return tspan_element.text
 
-    def fill_days_and_hours(self, month, year, hours_worked):
-        first_weekday, days_in_month = calendar.monthrange(year, month)
-        day_numbers = [0] * first_weekday + [*range(1, days_in_month + 1)]
-        day_numbers = day_numbers + [0] * (35 - len(day_numbers))
+    def fill_days_and_hours(self, hours_worked):
+        day_numbers = [0] * self.first_weekday + [*range(1, self.days_in_month + 1)]
+        day_numbers = day_numbers + [0] * (self.card_size - len(day_numbers))
         for i, day in enumerate(day_numbers, start=1):
             self.set_value(f'day_{i}', day or '')
             wh = hours_worked.get(day, None)
@@ -66,14 +76,14 @@ class WorkedHoursCardCreator:
         hours_worked_text = f'{_('Accounted hours')}: {sum(hours_worked.values())}'
         self.set_values('footer', [days_worked_text, hours_worked_text])
 
-    def generate_png_card(self, subdivision, full_name, tab_no, month, year, hours_worked=None):
-        date = datetime(year, month, 1)
+    def generate_png_card(self, subdivision, full_name, tab_no, hours_worked=None):
+        date = datetime(self.year, self.month, 1)
         month_name = format_date(date, "LLLL", locale=self.locale).capitalize()
 
-        self.set_value('month_year', f'{month_name} {year}')
+        self.set_value('month_year', f'{month_name} {self.year}')
         self.set_values('employee', [subdivision, tab_no, full_name])
         self.fill_day_names()
-        self.fill_days_and_hours(month, year, hours_worked)
+        self.fill_days_and_hours(hours_worked)
         self.fill_footer(hours_worked)
 
         svg_xml = etree.tostring(self.root, xml_declaration=True, encoding='UTF-8')
